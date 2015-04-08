740ba51de61df4e51ad03d855575e59d11053857
denniscardoso <dennis.cardoso.122@gmail.com>
2015-04-08 00:33:30 -0500

    Account Class updated

--- app/src/main/java/edu/illinois/dscs/mypocket/model/Account.java
+++ app/src/main/java/edu/illinois/dscs/mypocket/model/Account.java
@@ -1,7 +1,38 @@
 package edu.illinois.dscs.mypocket.model;
 
+import java.util.ArrayList;
+
 /**
- * Created by Cassio on 07/04/2015.
+ * Created by Dennis on 07/04/2015.
  */
 public class Account {
+
+    private String name;
+    private ArrayList<Transaction> transactionList;
+
+
+    /**
+     * Creates a new transaction object.
+     *
+     * @param name the name of the transaction.
+     */
+    public Account(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void removeTransaction(){
+         transactionList.remove();
+    }
+
+    public void addTransaction(){
+        
+    }
 }
