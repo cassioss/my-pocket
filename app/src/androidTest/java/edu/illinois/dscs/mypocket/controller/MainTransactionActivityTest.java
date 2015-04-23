7cf6adc21e1ab3ba519fce0b4f094ca44d76dc94
Cassio Sousa <cassiosantossousa@gmail.com>
2015-04-23 10:36:55 -0500

    Refactored unit test

--- app/src/androidTest/java/edu/illinois/dscs/mypocket/controller/MainTransactionActivityTest.java
+++ app/src/androidTest/java/edu/illinois/dscs/mypocket/controller/MainTransactionActivityTest.java
@@ -4,9 +4,6 @@ import android.app.Instrumentation.ActivityMonitor;
 import android.test.ActivityUnitTestCase;
 import android.widget.Button;
 
-import org.junit.Before;
-import org.junit.Test;
-
 import edu.illinois.dscs.mypocket.R;
 
 /**
@@ -22,25 +19,17 @@ public class MainTransactionActivityTest extends ActivityUnitTestCase<MainActivi
         super(activityClass);
     }
 
-    @Before
     public void setUp() throws Exception {
         super.setUp();
         mainActivity = getActivity();
         mAddTransactionButton = (Button) mainActivity.findViewById(R.id.add_transaction_button);
     }
 
-    @Test
     public void testGoingToAddTransaction() {
         // register next activity that need to be monitored.
         ActivityMonitor activityMonitor = getInstrumentation().addMonitor(AddTransactionActivity.class.getName(), null, false);
 
-        mainActivity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                // click button and open next activity.
-                mAddTransactionButton.performClick();
-            }
-        });
+        mainActivity.runOnUiThread(mAddTransactionButton::performClick);
 
         AddTransactionActivity addTransactionActivity = (AddTransactionActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 5000);
         assertNotNull(addTransactionActivity);
